cfbbb8e3e6b95abca9c2e21b2afb0c54a5b55de4
Node Coding Exercise <arikorycooper@gmail.com>
2018-04-04 14:14:04 -0700

    it works!

--- bot.js
+++ bot.js
@@ -1,11 +1,4 @@
 console.log('The bot is starting');
-
-//// set up port
-//var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 3000,
-//    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-//    
-//this.listen(port, ip);
-//console.log('Server running on http://%s:%s', ip, port);
     
 var Twit = require('twit');
 var config = require('./config.js');
@@ -16,11 +9,6 @@ var stream = T.stream('statuses/filter', { track: '#MAGA', language: 'en' });
 stream.on('tweet', function(tweet) {
     tweetEvent(tweet);
 });
-
-//var params = {
-//    q: 'trump',
-//    count: 5
-//};
 
 function tweetEvent(tweet) {
     console.log(tweet.text);
@@ -33,18 +21,15 @@ function tweetEvent(tweet) {
 function tweetIt(from) {
 
     //post to twitter with tweet
-    var tweet = {
-        status: 'Thanks, ' + '@' + from + ',  for keeping it trashy! #TrumpDump'
-    };
-
-    T.post('status/update', tweet, tweeted);
-
-
-    function tweeted(err, data, response) {
+    var post = 'Thanks, ' + '@' + from + ',  for keeping it trashy! #TrumpDump';
+    var other = 'covfefe';
+    T.post('statuses/update', { status: post }, function(err, data, response) {
         if (err) {
-           console.log('err= ' + err)
+           console.log('***** '+err+' *****');
         } else {
-            console.log('posted: \n\ ' + data);
+            console.log('***** posted: \n\ ' + data);
         }
-    }
+    });
+
+
 }
