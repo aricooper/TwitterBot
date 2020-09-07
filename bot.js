59501df408ef027f5ebd3df7930dbe17e82544ce
Ari Kory Cooper <arikorycooper@gmail.com>
2020-09-07 11:07:58 -0700

    FuckWithTuckerCarlsonBot
    Changed the bot to reply to Tucker Carlson posts.

--- bot.js
+++ bot.js
@@ -4,32 +4,23 @@ var Twit = require('twit');
 var config = require('./config.js');
 var T = new Twit(config);
 
-var stream = T.stream('statuses/filter', { track: '#MAGA', language: 'en' });
+var stream = T.stream('statuses/filter', { from: 'TuckerCarlson', lang: 'en'});
 
 stream.on('tweet', function(tweet) {
     tweetEvent(tweet);
 });
 
 function tweetEvent(tweet) {
-    console.log(tweet.text);
-    var from = tweet.user.screen_name;
-    tweetIt(from);
+    var reply = {
+    status: "Hi, I'm @" + tweet.user.screen_name + " and I have used my elite privilege to consistently divide Americans, weaponizing fear and racism to divert the accumulated frustration of the working class from the systemic and pernicious effects of unfettered capitalism and white supremacy. Don't believe me? Just look at my most recent deceit above.",
+    in_reply_to_status_id: '' + tweet.id_str
+    };
 
-}
-//setInterval(tweetIt, 1000 * 60);
-
-function tweetIt(from) {
-
-    //post to twitter with tweet
-    var post = 'Thanks, ' + '@' + from + ',  for keeping it trashy! #TrumpDump';
-    var other = 'covfefe';
-    T.post('statuses/update', { status: post }, function(err, data, response) {
+    T.post('statuses/update', reply, function(err, data, response) {
         if (err) {
            console.log('***** '+err+' *****');
         } else {
             console.log('***** posted: \n\ ' + data);
         }
     });
-
-
 }
--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
     
   consumer_key:         'e0oFeHBMkwaQOgntUHZkiFZF9',
   consumer_secret:      'CIisTnB1zFXjR6sEXDTnMdS9Gk54imN3MxqzhYSYAsWygAFXuC',
-  access_token:         '973634008721506304-4p6R44D0hSDiP27EyPX3vNl6XyQ0fhZ',
-  access_token_secret:  'bTQKA6VMeEMoMGUvWjIO80cot7gSRuBcldZtui4kHPgIP'
+  access_token:         '973634008721506304-3oD69k4IWSygu3dhQYkfc9tVpOzclkM',
+  access_token_secret:  '3aGqPnMCxK2IGLGNfqb0PugKzfrr5HLkTyEvwzPLzbrrb'
 
 };
